6043008ed32b4035968a0d55996099670f5f12b4
Rene Hexel <r.hexel@griffith.edu.au>
2018-12-11 15:38:43 +1000

    Update for swift-tools-version:4

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription 
 
 let package = Package(name: "CGLib",
 	pkgConfig: "glib-2.0 gio-unix-2.0",
 	providers: [
-		.Brew("glib glib-networking gobject-introspection"),
-		.Apt("libglib2.0-dev glib-networking gobject-introspection libgirepository1.0-dev")
+		.brew("glib glib-networking gobject-introspection"),
+		.apt("libglib2.0-dev glib-networking gobject-introspection libgirepository1.0-dev")
 	]
 )
